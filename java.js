5b89fd5ec53adf72898fc0bc383c206625adc6f6
inessev <123724101+inessev@users.noreply.github.com>
2023-04-19 16:46:00 +0200

    prueba 4

--- java.js
+++ java.js
@@ -1,8 +1,11 @@
-const nombre = prompt("¿Cuál es tu nombre?");
+alert ("Escriba dos números para obtener el resultado de su suma, resta, multiplicación y división");
+var name1;
+name1 = parseInt(prompt('Escriba aquí su nombre'));
 let nombreAlReves = "";
 
 for (let i = nombre.length - 1; i >= 0; i--) {
-  nombreAlReves += nombre[i];
+  nombreAlReves += name1[i];
 }
 
 alert("¡Adiós " + nombreAlReves + "!" );
+
